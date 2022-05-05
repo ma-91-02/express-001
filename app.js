f577c969eee2b5faf9186f3dbb0a5d55c1db09f8
ma-91-02 <ma.91.02@mail.ru>
2022-05-05 07:20:34 +0300

    adding templation engines

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const adminRoutes = require("./routes/admin");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+const adminData = require("./routes/admin");
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path');
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+// to join with file like css
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/admin',adminRoutes);
+app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,22 +1,30 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
-const rootDir = require('../util/path');
+const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 /***********************************
-***   /admin/add-product =>GET   ***
-************************************/  
+ ***   /admin/add-product =>GET   ***
+ ************************************/
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir,'views','add-product.html'));
+//   res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    res.render('add-product', {pageTitle:'Add Product'})
 });
 
 /*************************************
-***   /admin/add-product => POST   ***
-*************************************/
+ ***   /admin/add-product => POST   ***
+ *************************************/
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+    const obj = JSON.parse(JSON.stringify(req.body.title));
+//   console.log(obj);
+  products.push({title:obj});
+  console.log(products);
+//   console.log(products);
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
